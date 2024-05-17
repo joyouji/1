90bba419c18d4accf08a504964e680af586eafb5
minemine0961 <park100056@gmail.com>
2024-05-17 16:20:02 +0900

    survey and exercise

--- src/main/java/security/loginsecurity/controller/HomeController.java
+++ src/main/java/security/loginsecurity/controller/HomeController.java
@@ -2,24 +2,58 @@ package security.loginsecurity.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import security.loginsecurity.exercise.ExerciseService;
 import security.loginsecurity.home.service.CalendarService;
+import security.loginsecurity.member.Member;
+import security.loginsecurity.repository.MemberRepository;
 
 import java.time.LocalDate;
 
 @Controller
 public class HomeController {
-    @Autowired
-    private CalendarService calendarService;
 
+    private final CalendarService calendarService;
+    private final MemberRepository memberRepository;
+    private final ExerciseService exerciseService;
+
+    @Autowired
+    public HomeController(CalendarService calendarService, MemberRepository memberRepository, ExerciseService exerciseService) {
+        this.calendarService = calendarService;
+        this.memberRepository = memberRepository;
+        this.exerciseService = exerciseService;
+    }
+
+    /*
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/home";
+    }
+    */
     @GetMapping("/")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView("/home");
         return modelAndView;
+    }
+
+    @GetMapping("/home")
+    public String homePage(Model model, Authentication authentication) {
+
+        String email = authentication.getName();
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        model.addAttribute("exercise", exerciseService.getExerciseToday(member));
+        model.addAttribute("goalToday", exerciseService.getGoalToday(member));
+
+        // home.html 띄우기
+        return "home";
     }
 
     @GetMapping("/memo")
